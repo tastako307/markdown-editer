d6acfcb8b6846664a347ffa872f935fd81a96744
tastako307 <tastako307@gmail.com>
2020-12-13 21:42:52 +0900

    入力エリアの設定TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import styled from 'styled-components'
-const Header = styled.h1`
-  color:red;
+import { createGlobalStyle } from 'styled-components'
+import { Editor } from './pages/editor'
+
+const GlobalStyle = createGlobalStyle`
+body * {
+  box-sizing: border-box;
+}
 `
-const Main = (<Header>Markdown Editor </Header>)
+
+const Main = (
+<>
+  <GlobalStyle />
+  <Editor />
+</>
+)
 render(Main, document.getElementById('app'))
